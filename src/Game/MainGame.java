229e3c492fe95ee9f93a7d5093c6d43f5d4d1381
31338283 <31338283@093073-P31S405.macklabs.br>
2015-08-27 08:25:12 -0300

    Criando exception para game

--- src/Game/MainGame.java
+++ src/Game/MainGame.java
@@ -16,31 +16,33 @@ public class MainGame {
     public MainGame(Gamer gamer) {
         this.gamer = gamer;
         boolean cont = true;
-        do{
+        do {
             String resp = IOManager.readKey("Quer jogar? (S/N)");
-            if(resp.equalsIgnoreCase("S")){
+            if (resp.equalsIgnoreCase("S")) {
                 gameLogic();
             } else {
                 cont = false;
             }
-        }while(cont);
-        
+        } while (cont);
+
         String resp2 = IOManager.readKey("Quer salvar o seu jogo? (S/N)");
-        if(resp2.equalsIgnoreCase("S")){
-            
+        if (resp2.equalsIgnoreCase("S")) {
+
             //seta last score
             gamer.setLast_score(gamer.getCurrent_score());
-            
+
             //verifica se current e  maior que max
-            if(gamer.getCurrent_score() > gamer.getMax_score()) gamer.setMax_score(gamer.getCurrent_score());
-            
+            if (gamer.getCurrent_score() > gamer.getMax_score()) {
+                gamer.setMax_score(gamer.getCurrent_score());
+            }
+
             //save
             File file = new File("scoreData.txt");
             IOManager.writeGamer(gamer, file);
             System.out.println("Seu jogo foi salvo com sucesso");
         }
         System.out.println("Xau!");
-        
+
     }
 
     public int randomize(int min, int max) {
@@ -51,12 +53,19 @@ public class MainGame {
         randomInt = randomize(0, 10);
 
         System.out.println("numero gerado " + randomInt);
-        int gamerInt = Integer.parseInt(IOManager.readKey("Adivinha meu numero"));
-        if (gamerInt == randomInt) {
-            System.out.println("ACERTOU!");
-            gamer.incrementScore();
-        } else {
-            System.out.println("EROU!");
+        try {
+            int gamerInt = Integer.parseInt(IOManager.readKey("Adivinha meu numero"));
+            if(gamerInt < 0 || gamerInt > 10){
+                throw new ForaLimiteException("Voce deve digitar um valor entre 0 e 10");
+            }
+            if (gamerInt == randomInt) {
+                System.out.println("ACERTOU!");
+                gamer.incrementScore();
+            } else {
+                System.out.println("EROU!");
+            }
+        } catch (NumberFormatException ex) {
+            System.out.println("Voce deve digitar um valor numerico");
         }
         System.out.println("voce tem: " + gamer.getCurrent_score() + " pontos!");
     }
